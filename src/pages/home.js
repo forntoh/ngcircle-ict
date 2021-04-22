3473b9997e2e8df92bb04684b67242b746bda174
Forntoh Thomas <thomasforntoh@gmail.com>
2021-04-22 11:58:20 +0200

    finished home

--- src/pages/home.js
+++ src/pages/home.js
@@ -7,11 +7,28 @@ import { Header } from "../components/header/header";
 import { Footer } from "../components/footer/footer";
 import { CallToAction } from "../components/callToAction/callToaction";
 
+import home_left from "../assets/images/home_left.svg";
+import home_right from "../assets/images/home_right.svg";
+
+import "./home.css";
+
 export const HomePage = () => (
   <div>
     <Header />
     <CallToAction description="PERSÖNLICHE VIELFALT SCHAFFT DIVERSITÄT" />
-    <div className="primary-image"></div>
+    <div className="primary-image">
+      <img src={home_left} />
+      <div className="right">
+        <div>
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+          nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+          sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
+          rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
+          ipsum dolor sit amet.
+        </div>
+        <img src={home_right} />
+      </div>
+    </div>
     <div className="content-wrapper">
       <span>LOREM IPSUM DOLOR SIT AMET</span>
       <p>
