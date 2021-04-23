0d960967c33bb6934ef3e579eb06a37477af3d9d
Forntoh Thomas <thomasforntoh@gmail.com>
2021-04-23 10:54:30 +0200

    test hash router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Router } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 import "./App.css";
 
@@ -8,9 +8,9 @@ import Routes from "./routes";
 
 function App() {
   return (
-    <Router history={history}>
+    <HashRouter history={history}>
       <Routes />
-    </Router>
+    </HashRouter>
   );
 }
 
