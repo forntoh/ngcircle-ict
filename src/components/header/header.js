c8474401d828cd6c10383818555c3977fa2e6640
Forntoh Thomas <thomasforntoh@gmail.com>
2021-04-23 17:41:12 +0200

    added overflow menus

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -3,19 +3,44 @@ import React from "react";
 import { Button } from "../button/button";
 import { Logo } from "../logo/logo";
 
-import "./header.css";
+import "./header.scss";
 
 export const Header = () => (
   <header>
     <div className="wrapper">
       <Logo type="full" />
       <div style={{ flexGrow: 1 }}></div>
-      <Button label="INNOVATIONEN" destination="/innovationen" />
-      <Button label="FÖRDERUNGEN" destination="/forderungen" />
-      <Button label="UNTERNEHMEN" destination="/unternehmen" />
-      <Button label="LEISTUNGEN" destination="/leistungen" />
-      <Button label="KARRIERE" destination="/karriere" />
-      <Button label="KONTAKT" destination="/kontakt" />
+      <ul className="nav-items">
+        <li>
+          <Button label="INNOVATIONEN" destination="/innovationen" />
+        </li>
+        <li>
+          <Button label="FÖRDERUNGEN" destination="/forderungen" />
+        </li>
+        <li>
+          <span>UNTERNEHMEN</span>
+          <div className="overflow">
+            <Button label="ÜBER UNS" destination="/uber" />
+            <Button label="AUSZEICHNUNGEN" destination="/auszeichnungen" />
+            <Button label="FIRMENPHILOSOPHIE" destination="/philosophie" />
+            <Button label="NETZWERK" destination="/netzwerk" />
+          </div>
+        </li>
+        <li>
+          <span>UNTERNEHMEN</span>
+          <div className="overflow">
+            <Button label="1TNC" destination="/1tnc" />
+            <Button label="NEW GLOBE CENTER" destination="/ngc" />
+            <Button label="DIGITAL SOLUTIONS" destination="/digi-solutions" />
+          </div>
+        </li>
+        <li>
+          <Button label="KARRIERE" destination="/karriere" />
+        </li>
+        <li>
+          <Button label="KONTAKT" destination="/kontakt" />
+        </li>
+      </ul>
     </div>
   </header>
 );
