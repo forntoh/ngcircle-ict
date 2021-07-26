16018ebdc445f823a0e1e4cb8f14c0526d6425b7
Forntoh Thomas <thomasforntoh@gmail.com>
2021-07-26 12:42:38 +0200

    changed bottom section titles

--- src/pages/home.js
+++ src/pages/home.js
@@ -53,19 +53,19 @@ export const HomePage = () => (
     </div>
     <div className="wrapper-values">
       <ValueItem
-        title="LOREM IPSUM"
+        title="Unternehmen"
         description={[
           "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
         ]}
       />
       <ValueItem
-        title="LOREM IPSUM"
+        title="Presse"
         description={[
           "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr:",
         ]}
       />
       <ValueItem
-        title="LOREM IPSUM"
+        title="Karriere"
         description={[
           "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr:",
         ]}
